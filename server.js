0b1fa0a36a716df42ce06ceeb2fa298c8565f436
Jason Seminara <jseminara@gmail.com>
2016-07-14 12:38:05 -0400

    split off routes

--- server.js
+++ server.js
@@ -7,23 +7,19 @@ const path        = require('path');
 const app         = express();
 const PORT        = process.argv[2] || process.env.port || 3009;
 
+const taskRoutes  = require('./routes/tasks');
 
 // set up some logging
 app.use( logger( 'dev') );
+
+app.use( '/tasks', taskRoutes );
 
 // Let's go!
 app.listen(PORT , ()=>
   console.log(`server here! listening on`, PORT )
 )
 
-app.route('/tasks/:id')
-  .get((req,res)=>res.send(`showed task ${req.params.id}`))
-  .put((req,res)=>res.send(`edited task ${req.params.id}`))
-  .delete((req,res)=>res.send(`deleted task ${req.params.id}`))
 
-app.route('/tasks')
-  .get((req,res)=>res.send('show tasks'))
-  .post((req,res)=>res.send('posted new task'))
 
 
 app.get('/', (req,res)=>{
