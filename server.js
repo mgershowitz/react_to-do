cb5a83262901d35cf341c5a523d40c7818c011f8
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-07-14 14:49:12 -0400

    created env

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 'ues strict'
 
-const express = require('express')
-const morgan  = require('morgan')
-const path    = require('path')
-//const favicon = require('serve-favicon');
-const bodyParser = require('body-parser')
-const app     = express()
-const taskRoute = require('./routes/tasks')
-const port    = process.argv[2] || process.env.PORT || 3009
+const env         = process.env.NODE_ENV || 'development';
+const DEV         = env === 'development';
+const dotenv      = (DEV) ? require('dotenv').config() : undefined;
+const express     = require('express')
+const morgan      = require('morgan')
+const path        = require('path')
+const bodyParser  = require('body-parser')
+const app         = express()
+const taskRoute   = require('./routes/tasks')
+const port        = process.argv[2] || process.env.PORT || 3009
 
 app.use(morgan('dev'))
 app.set(express.static(path.join(__dirname,'dist')))
 app.use(bodyParser.json());
-//app.use(express.favicon(path.join(__dirname, 'public','images')));
 
 app.listen(port, ()=>{
   console.log('Server chillin at ', port)
