a5909ebbadec53e30763b284f63483537c50f5eb
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-07-14 13:49:06 -0400

    routes made before lunch

---
+++ routes/tasks.js
@@ -0,0 +1,17 @@
+'use strict'
+const express = require('express')
+const tasks   = express.Router();
+
+let taskList = [];
+
+tasks.route('/:id')
+  .get((req,res) => res.json(tasks))
+  .put((req,res) => res.json(tasks))
+  .delete((req,res) => res.json(tasks))
+
+tasks.route('/')
+  .get((req,res) => {res.json(res.rows)})
+  .post((req,res) => {res.json(tasks)})
+
+
+module.exports = tasks;
--- server.js
+++ server.js
@@ -4,25 +4,23 @@ const express = require('express')
 const morgan  = require('morgan')
 const path    = require('path')
 //const favicon = require('serve-favicon');
+const bodyParser = require('body-parser')
 const app     = express()
+const taskRoute = require('./routes/tasks')
 const port    = process.argv[2] || process.env.PORT || 3009
 
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,'dist')))
+app.set(express.static(path.join(__dirname,'dist')))
+app.use(bodyParser.json());
 //app.use(express.favicon(path.join(__dirname, 'public','images')));
 
 app.listen(port, ()=>{
   console.log('Server chillin at ', port)
 })
 
-app.route('/tasks/:id')
-  .get((req,res) => res.send(`show task ${req.params.id}`))
-  .put((req,res) => res.send(`edit task ${req.params.id}`))
-  .delete((req,res) => res.send(`delete task ${req.params.id}`))
 
-app.route('/tasks')
-  .get((req,res) => {res.send('show tasks')})
-  .post((req,res) => {res.send('posted new task')})
+app.use('/tasks', taskRoute)
+
 
 app.get('/', (req,res)=>{
   res.send('home')
