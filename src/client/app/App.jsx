c0eac5896af5e04d7f61046510deee0f428bb7b9
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-07-15 17:08:55 -0400

    ajax calls werk

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -8,6 +8,8 @@ import Footer           from './Footer.jsx'
 import TaskForm         from './TaskForm.jsx'
 import TaskList         from './TaskList.jsx'
 
+import ajax             from '../helpers/ajaxAdapter.js'
+import util             from '../helpers/util.js'
 // create a React Component called _App_
 export default class App extends React.Component{
 
@@ -26,31 +28,40 @@ export default class App extends React.Component{
     // this is right fetr the component is mounted on the screen
     componentDidMount(){
         //go to the database and get the freshest tasks
-
+        ajax.getTasks().then( data =>
+            this.setState({tasks: data.indexByKey('task_id')})
         //when the data comes back, update the state
+    )
     }
 
 
 
     // note that classes do **not** have commas between their methods
     addTask( newTask ){
+        ajax.createTask(newTask).then(data=>{
+            this.state.tasks[ data[0].task_id ] = data[0]
+            this.setState({tasks: this.state.tasks})
+        })
         //send this change to the db (ajax)
-        newTask.task_name = newTask.name
-        newTask.task_desc = newTask.desc
-        newTask.completed = false;
-        newTask.task_id = Date.now();
+
+
 
         //when the data comes back, update the state
-        this.state.tasks[newTask.task_id] = newTask;
-        this.setState({tasks: this.state.tasks})
+
 
     }
 
     toggleTask( key ){
-        this.state.tasks[key].completed = !this.state.tasks[key].completed;
+        let myTask = this.state.tasks[key];
+
+        myTask.completed = !myTask.completed;
+        ajax.updateTask(myTask)
+            .then(data=>{
+                this.state.tasks[data.task_id] = data
+                this.setState({tasks: this.state.tasks})
+            })
         //send out this new change to the DB (ajax)
         //bring in AJAX data here!
-        this.setState({tasks: this.state.tasks})
     }
     // 90% of your components will render()
     // REMEMBER you can only return **one** root element from a render fn.
