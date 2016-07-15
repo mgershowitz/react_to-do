32f965142b6259f7b9ada35adad90206cea1a792
Jason Seminara <jseminara@gmail.com>
2016-07-15 15:15:38 -0400

    exercise 7

---
+++ src/client/app/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+require('../images/GA_Stack_Small_RedBlack_RGB.png')
+
+const Footer = props=>
+  <footer>
+    <div className="footer-copyright">
+      <div className="container">
+        <p>Made with <span className="glyphicon glyphicon-heart"></span> by students of <span className="gaLogo"></span></p>
+      </div>
+    </div>
+  </footer>
+//write your function here
+
+export default Footer;
